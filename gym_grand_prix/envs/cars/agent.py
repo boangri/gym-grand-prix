6bc331c51b7e05642dbc45d225a7b6e4c27abef8
boris@acer <xinu@yandex.ru>
2020-08-13 12:01:39 +0300

    Added source code of GrandPrix pygame

---
+++ gym_grand_prix/envs/cars/agent.py
@@ -0,0 +1,161 @@
+import random
+from abc import ABCMeta, abstractmethod
+from collections import deque
+
+import numpy as np
+
+from cars.utils import Action
+from learning_algorithms.network import Network
+
+
+class Agent(metaclass=ABCMeta):
+    @property
+    @abstractmethod
+    def rays(self):
+        pass
+
+    @abstractmethod
+    def choose_action(self, sensor_info):
+        pass
+
+    @abstractmethod
+    def receive_feedback(self, reward):
+        pass
+
+
+class SimpleCarAgent(Agent):
+    def __init__(self, history_data=int(500000)):
+        """
+        Создаёт машинку
+        :param history_data: количество хранимых нами данных о результатах предыдущих шагов
+        """
+        self.evaluate_mode = False  # этот агент учится или экзаменутеся? если учится, то False
+        self._rays = 5  # выберите число лучей ладара; например, 5
+        # here +2 is for 2 inputs from elements of Action that we are trying to predict
+        self.neural_net = Network([self.rays + 4,
+                                   100,  # внутренние слои сети: выберите, сколько и в каком соотношении вам нужно
+                                   10,  # например, (self.rays + 4) * 2 или просто число
+                                   1],
+                                  output_function=lambda x: x, output_derivative=lambda x: 1)
+        self.sensor_data_history = deque([], maxlen=history_data)
+        self.chosen_actions_history = deque([], maxlen=history_data)
+        self.reward_history = deque([], maxlen=history_data)
+        self.step = 0
+        self.avg_reward = 0.    # средняя награда за последнюю тысячу шагов
+        self.sum_reward = 0.    # сумма всех наград
+
+    @classmethod
+    def from_weights(cls, layers, weights, biases):
+        """
+        Создание агента по параметрам его нейронной сети. Разбираться не обязательно.
+        """
+        agent = SimpleCarAgent()
+        agent._rays = weights[0].shape[1] - 4
+        nn = Network(layers, output_function=lambda x: x, output_derivative=lambda x: 1)
+
+        if len(weights) != len(nn.weights):
+            raise AssertionError("You provided %d weight matrices instead of %d" % (len(weights), len(nn.weights)))
+        for i, (w, right_w) in enumerate(zip(weights, nn.weights)):
+            if w.shape != right_w.shape:
+                raise AssertionError("weights[%d].shape = %s instead of %s" % (i, w.shape, right_w.shape))
+        nn.weights = weights
+
+        if len(biases) != len(nn.biases):
+            raise AssertionError("You provided %d bias vectors instead of %d" % (len(weights), len(nn.weights)))
+        for i, (b, right_b) in enumerate(zip(biases, nn.biases)):
+            if b.shape != right_b.shape:
+                raise AssertionError("biases[%d].shape = %s instead of %s" % (i, b.shape, right_b.shape))
+        nn.biases = biases
+
+        agent.neural_net = nn
+
+        return agent
+
+    @classmethod
+    def from_string(cls, s):
+        from numpy import array  # это важный импорт, без него не пройдёт нормально eval
+        layers, weights, biases = eval(s.replace("\n", ""), locals())
+        return cls.from_weights(layers, weights, biases)
+
+    @classmethod
+    def from_file(cls, filename):
+        c = open(filename, "r").read()
+        return cls.from_string(c)
+
+    def show_weights(self):
+        params = self.neural_net.sizes, self.neural_net.weights, self.neural_net.biases
+        #np.set_printoptions(threshold=np.nan)
+        np.set_printoptions(threshold=1e9)
+        return repr(params)
+
+    def to_file(self, filename):
+        c = self.show_weights()
+        f = open(filename, "w")
+        f.write(c)
+        f.close()
+
+    @property
+    def rays(self):
+        return self._rays
+
+    def choose_action(self, sensor_info):
+        # хотим предсказать награду за все действия, доступные из текущего состояния
+        rewards_to_controls_map = {}
+        # дискретизируем множество значений, так как все возможные мы точно предсказать не сможем
+        all_actions = ((0., 0.), (0., .75), (0., -.75), (1., .75), (-1., .75))
+        probabilities = np.zeros(5)
+        s = 0.  # sum for softmax
+        ind = 0
+        for steering, acceleration in all_actions:
+            action = Action(steering, acceleration)
+            agent_vector_representation = np.append(sensor_info, action)
+            agent_vector_representation = agent_vector_representation.flatten()[:, np.newaxis]
+            predicted_reward = float(self.neural_net.feedforward(agent_vector_representation))
+            probabilities[ind] = np.exp(predicted_reward)
+            s += probabilities[ind]
+            ind += 1
+        probabilities /= s
+        ind = np.random.choice(5, size=1, p=probabilities)[0]
+        steering, acceleration = all_actions[ind]
+        best_action = Action(steering, acceleration)
+        # запомним всё, что только можно: мы хотим учиться на своих ошибках
+        self.sensor_data_history.append(sensor_info)
+        self.chosen_actions_history.append(best_action)
+        self.reward_history.append(0.0)  # мы пока не знаем, какая будет награда, это
+        # откроется при вызове метода receive_feedback внешним миром
+
+        return best_action
+
+    def receive_feedback(self, reward, train_every=100, reward_depth=10):
+        """
+        Получить реакцию на последнее решение, принятое сетью, и проанализировать его
+        :param reward: оценка внешним миром наших действий
+        :param train_every: сколько нужно собрать наблюдений, прежде чем запустить обучение на несколько эпох
+        :param reward_depth: на какую глубину по времени распространяется полученная награда
+        """
+        # считаем время жизни сети; помогает отмерять интервалы обучения
+        self.step += 1
+        q = .001 if self.step > 1000 else 1./float(self.step)
+        self.avg_reward = (1. - q)*self.avg_reward + q*reward
+        self.sum_reward += reward
+
+        # начиная с полной полученной истинной награды,
+        # размажем её по предыдущим наблюдениям
+        # чем дальше каждый раз домножая её на 1/2
+        # (если мы врезались в стену - разумно наказывать не только последнее
+        # действие, но и предшествующие)
+        i = -1
+        while len(self.reward_history) > abs(i) and abs(i) < reward_depth:
+            self.reward_history[i] += reward
+            reward *= 0.9
+            i -= 1
+
+        # Если у нас накопилось хоть чуть-чуть данных, давайте потренируем нейросеть
+        # прежде чем собирать новые данные
+        # (проверьте, что вы в принципе храните достаточно данных (параметр `history_data` в `__init__`),
+        # чтобы условие len(self.reward_history) >= train_every выполнялось
+        if not self.evaluate_mode and (len(self.reward_history) >= train_every) and not (self.step % train_every):
+            X_train = np.concatenate([self.sensor_data_history, self.chosen_actions_history], axis=1)[-10*train_every:]
+            y_train = np.array(self.reward_history)[-10*train_every:]
+            train_data = [(x[:, np.newaxis], y) for x, y in zip(X_train, y_train)]
+            self.neural_net.SGD(training_data=train_data, epochs=15, mini_batch_size=train_every, eta=0.05)
---
+++ gym_grand_prix/envs/cars/world.py
@@ -0,0 +1,346 @@
+import itertools
+import random
+from abc import ABCMeta, abstractmethod
+from cmath import rect, pi, phase
+from time import sleep
+
+import numpy as np
+import pygame
+
+from cars.agent import SimpleCarAgent
+from cars.track import plot_map
+from cars.utils import CarState, to_px, rotate, intersect_ray_with_segment, draw_text, angle
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+
+
+class World(metaclass=ABCMeta):
+    @abstractmethod
+    def transition(self):
+        pass
+
+    @abstractmethod
+    def run(self):
+        pass
+
+
+class SimpleCarWorld(World):
+    # Научиться не врезаться в стенки
+    COLLISION_PENALTY = 32 * 1e0
+    HEADING_REWARD = 10 * 1e0
+    WRONG_HEADING_PENALTY = 0 * 1e0
+    IDLENESS_PENALTY = 32 * 1e-1
+    SPEEDING_PENALTY = 0 * 1e-1
+    MIN_SPEED = 0.1 * 1e0
+    MAX_SPEED = 10 * 1e0
+    # Подружиться с педалью газа (и тормоза)
+    #     COLLISION_PENALTY = 0 * 1e0
+    #     HEADING_REWARD = 0 * 1e-1
+    #     WRONG_HEADING_PENALTY = 0 * 1e0
+    #     IDLENESS_PENALTY = 32 * 1e-1
+    #     SPEEDING_PENALTY = 32 * 1e-1
+    #     MIN_SPEED = 0.1 * 1e0
+    #     MAX_SPEED = 0.7 * 1e0
+
+    size = (800, 600)
+
+    def __init__(self, num_agents, car_map, Physics, agent_class, window=True, **physics_pars):
+        """
+        Инициализирует мир
+        :param num_agents: число агентов в мире
+        :param car_map: карта, на которой всё происходит (см. track.py0
+        :param Physics: класс физики, реализующий столкновения и перемещения
+        :param agent_class: класс агентов в мире
+        :param physics_pars: дополнительные параметры, передаваемые в конструктор класса физики
+        (кроме car_map, являющейся обязательным параметром конструктора)
+        """
+        self.physics = Physics(car_map, **physics_pars)
+        self.map = car_map
+        self.visual = window
+
+        # создаём агентов
+        self.set_agents(num_agents, agent_class)
+
+        self._info_surface = pygame.Surface(self.size)
+
+    def set_agents(self, agents=1, agent_class=None):
+        """
+        Поместить в мир агентов
+        :param agents: int или список Agent, если int -- то обязателен параметр agent_class, так как в мир присвоятся
+         agents агентов класса agent_class; если список, то в мир попадут все агенты из списка
+        :param agent_class: класс создаваемых агентов, если agents - это int
+        """
+        pos = (self.map[0][0] + self.map[0][1]) / 2
+        vel = 0
+        heading = rect(-0.3, 1)
+
+        if type(agents) is int:
+            self.agents = [agent_class() for _ in range(agents)]
+        elif type(agents) is list:
+            self.agents = agents
+        else:
+            raise ValueError("Parameter agent should be int or list of agents instead of %s" % type(agents))
+
+        self.agent_states = {a: CarState(pos, vel, heading) for a in self.agents}
+        self.circles = {a: 0 for a in self.agents}
+
+        self._agent_surfaces = []
+        self._agent_images = []
+
+    def transition(self):
+        """
+        Логика основного цикла:
+         подсчёт для каждого агента видения агентом мира,
+         выбор действия агентом,
+         смена состояния
+         и обработка реакции мира на выбранное действие
+        """
+        for a in self.agents:
+            vision = self.vision_for(a)
+            action = a.choose_action(vision)
+            next_agent_state, collision = self.physics.move(
+                self.agent_states[a], action
+            )
+            self.circles[a] += angle(self.agent_states[a].position, next_agent_state.position) / (2*pi)
+            self.agent_states[a] = next_agent_state
+            a.receive_feedback(self.reward(next_agent_state, collision, vision))
+
+    def reward(self, state, collision, sensor_info):
+        """
+        Вычисление награды агента, находящегося в состоянии state.
+        Эту функцию можно (и иногда нужно!) менять, чтобы обучить вашу сеть именно тем вещам, которые вы от неё хотите
+        :param sensor_info:
+        :param state: текущее состояние агента
+        :param collision: произошло ли столкновение со стеной на прошлом шаге
+        :return reward: награду агента (возможно, отрицательную)
+        """
+        V = sensor_info[0]
+        sin = -sensor_info[1]
+        heading_reward = V*sin
+        ladar = np.array(sensor_info[2:])
+        l = len(ladar)
+        n = int((l - 1) / 2)  # индекс в массиве ladar для направления вперед
+        s = ladar[n]  # расстояние до стенки прямо по курсу
+        speeding_penalty = abs(V*V - 0.45*s)
+        collision_penalty = int(collision) * self.COLLISION_PENALTY
+        reward = heading_reward * self.HEADING_REWARD - collision_penalty - speeding_penalty
+        # print("reward=%.3f" % reward)
+        return reward
+
+    def eval_reward(self, state, collision):
+        """
+        Награда "по умолчанию", используется в режиме evaluate
+        Удобно, чтобы не приходилось отменять свои изменения в функции reward для оценки результата
+        """
+        a = -np.sin(angle(-state.position, state.heading))
+        heading_reward = 1 if a > 0.1 else a if a > 0 else 0
+        heading_penalty = a if a <= 0 else 0
+        idle_penalty = 0 if abs(state.velocity) > self.MIN_SPEED else -self.IDLENESS_PENALTY
+        speeding_penalty = 0 if abs(state.velocity) < self.MAX_SPEED else -self.SPEEDING_PENALTY * abs(state.velocity)
+        collision_penalty = - max(abs(state.velocity), 0.1) * int(collision) * self.COLLISION_PENALTY
+
+        return heading_reward * self.HEADING_REWARD + heading_penalty * self.WRONG_HEADING_PENALTY + collision_penalty \
+            + idle_penalty + speeding_penalty
+
+    def run(self, steps=None):
+        """
+        Основной цикл мира; по завершении сохраняет текущие веса агента в файл network_config_agent_n_layers_....txt
+        :param steps: количество шагов цикла; до внешней остановки, если None
+        """
+        if self.visual:
+            scale = self._prepare_visualization()
+        for _ in range(steps) if steps is not None else itertools.count():
+            self.transition()
+            if self.visual:
+                self.visualize(scale)
+                if self._update_display() == pygame.QUIT:
+                    break
+            # sleep(0.1)
+
+        for i, agent in enumerate(self.agents):
+            try:
+                filename = "a_%d_layers_%s.txt" % (i, "_".join(map(str, agent.neural_net.sizes)))
+                agent.to_file(filename)
+                print("Saved agent parameters to '%s'" % filename)
+                print("Steps: %d Mean reward: %.3f Circles/1000steps: %.3f" %
+                      (agent.step, agent.sum_reward/agent.step, self.circles[agent]*1000/agent.step))
+            except AttributeError:
+                pass
+
+    def evaluate_agent(self, agent, steps=1000):
+        """
+        Прогонка цикла мира для конкретного агента (см. пример использования в комментариях после if _name__ == "__main__")
+        :param agent: SimpleCarAgent
+        :param steps: количество итераций цикла
+        :param visual: рисовать картинку или нет
+        :return: среднее значение награды агента за шаг
+        """
+        agent.evaluate_mode = True
+        self.set_agents([agent])
+        rewards = []
+        if self.visual:
+            scale = self._prepare_visualization()
+        for _ in range(steps):
+            vision = self.vision_for(agent)
+            action = agent.choose_action(vision)
+            next_agent_state, collision = self.physics.move(
+                self.agent_states[agent], action
+            )
+            self.circles[agent] += angle(self.agent_states[agent].position, next_agent_state.position) / (2*pi)
+            self.agent_states[agent] = next_agent_state
+            rewards.append(self.reward(next_agent_state, collision, vision))
+            agent.receive_feedback(rewards[-1])
+            if self.visual:
+                self.visualize(scale)
+                if self._update_display() == pygame.QUIT:
+                    break
+                # sleep(0.05)
+
+        return np.mean(rewards), self.circles[agent]
+
+    def vision_for(self, agent):
+        """
+        Строит видение мира для каждого агента
+        :param agent: машинка, из которой мы смотрим
+        :return: список из модуля скорости машинки, направленного угла между направлением машинки
+        и направлением на центр и `agent.rays` до ближайших стен трека (запустите картинку, и станет совсем понятно)
+        """
+        state = self.agent_states[agent]
+        vision = [abs(state.velocity), np.sin(angle(-state.position, state.heading))]
+        extras = len(vision)
+
+        delta = pi / (agent.rays - 1)
+        start = rotate(state.heading, - pi / 2)
+
+        sectors = len(self.map)
+        for i in range(agent.rays):
+            # define ray direction
+            ray = rotate(start, i * delta)
+
+            # define ray's intersections with walls
+            vision.append(np.infty)
+            for j in range(sectors):
+                inner_wall = self.map[j - 1][0], self.map[j][0]
+                outer_wall = self.map[j - 1][1], self.map[j][1]
+
+                intersect = intersect_ray_with_segment((state.position, ray), inner_wall)
+                intersect = abs(intersect - state.position) if intersect is not None else np.infty
+                if intersect < vision[-1]:
+                    vision[-1] = intersect
+
+                intersect = intersect_ray_with_segment((state.position, ray), outer_wall)
+                intersect = abs(intersect - state.position) if intersect is not None else np.infty
+                if intersect < vision[-1]:
+                    vision[-1] = intersect
+
+            assert vision[-1] < np.infty, \
+                "Something went wrong: {}, {}".format(str(state), str(agent.chosen_actions_history[-1]))
+        assert len(vision) == agent.rays + extras, \
+            "Something went wrong: {}, {}".format(str(state), str(agent.chosen_actions_history[-1]))
+        return vision
+
+    def visualize(self, scale):
+        """
+        Рисует картинку. Этот и все "приватные" (начинающиеся с _) методы необязательны для разбора.
+        """
+        for i, agent in enumerate(self.agents):
+            state = self.agent_states[agent]
+            surface = self._agent_surfaces[i]
+            rays_lengths = self.vision_for(agent)[-agent.rays:]
+            self._agent_images[i] = [self._draw_ladar(rays_lengths, state, scale),
+                                     self._get_agent_image(surface, state, scale)]
+
+        if len(self.agents) == 1:
+            a = self.agents[0]
+            draw_text("Reward: %.3f" % a.reward_history[-1], self._info_surface, scale, self.size,
+                      text_color=white, bg_color=black)
+            draw_text("Step: %d Avg reward: %.3f" % (a.step, a.avg_reward), self._info_surface, scale, self.size,
+                      text_color=white, bg_color=black, tlpoint=(self._info_surface.get_width() - 790, 10))
+            steer, acc = a.chosen_actions_history[-1]
+            state = self.agent_states[a]
+            draw_text("Action: steer.: %.2f, accel: %.2f" % (steer, acc), self._info_surface, scale,
+                      self.size, text_color=white, bg_color=black, tlpoint=(self._info_surface.get_width() - 500, 10))
+            draw_text("Inputs: |v|=%.2f, sin(angle): %.2f, circle: %.2f" % (
+                abs(state.velocity), np.sin(angle(-state.position, state.heading)), self.circles[a]),
+                      self._info_surface, scale,
+                      self.size, text_color=white, bg_color=black, tlpoint=(self._info_surface.get_width() - 500, 50))
+
+    def _get_agent_image(self, original, state, scale):
+        angle = phase(state.heading) * 180 / pi
+        rotated = pygame.transform.rotate(original, angle)
+        rectangle = rotated.get_rect()
+        rectangle.center = to_px(state.position, scale, self.size)
+        return rotated, rectangle
+
+    def _draw_ladar(self, sensors, state, scale):
+        surface = pygame.display.get_surface().copy()
+        surface.fill(white)
+        surface.set_colorkey(white)
+        start_pos = to_px(state.position, scale, surface.get_size())
+        delta = pi / (len(sensors) - 1)
+        ray = phase(state.heading) - pi / 2
+        for s in sensors:
+            end_pos = to_px(rect(s, ray) + state.position, scale, surface.get_size())
+            pygame.draw.line(surface, (0, 255, 0), start_pos, end_pos, 2)
+            ray += delta
+
+        rectangle = surface.get_rect()
+        rectangle.topleft = (0, 0)
+        return surface, rectangle
+
+    def _prepare_visualization(self):
+        red = (254, 0, 0)
+        pygame.init()
+        screen = pygame.display.set_mode(self.size)
+        screen.fill(white)
+        scale = plot_map(self.map, screen)
+        for state in self.agent_states.values():
+            s = pygame.Surface((25, 15))
+            s.set_colorkey(white)
+            s.fill(white)
+            pygame.draw.rect(s, red, pygame.Rect(0, 0, 15, 15))
+            pygame.draw.polygon(s, red, [(15, 0), (25, 8), (15, 15)], 0)
+            self._agent_surfaces.append(s)
+            self._agent_images.append([self._get_agent_image(s, state, scale)])
+
+        self._map_surface = screen
+        return scale
+
+    def _update_display(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return pygame.QUIT
+        display = pygame.display.get_surface()
+        display.fill(white)
+
+        plot_map(self.map, display)
+        for images in self._agent_images:
+            for surf, rectangle in images:
+                display.blit(surf, rectangle)
+        display.blit(self._info_surface, (0, 0), None, pygame.BLEND_RGB_SUB)
+        self._info_surface.fill(black)  # clear notifications from previous round
+        pygame.display.update()
+
+
+if __name__ == "__main__":
+    from HW_3.cars.physics import SimplePhysics
+    from HW_3.cars.track import generate_map
+
+    np.random.seed(3)
+    random.seed(3)
+    m = generate_map(8, 5, 3, 3)
+    SimpleCarWorld(1, m, SimplePhysics, SimpleCarAgent, timedelta=0.2).run()
+
+    # если вы хотите продолжить обучение уже существующей модели, вместо того,
+    # чтобы создавать новый мир с новыми агентами, используйте код ниже:
+    # # он загружает агента из файла
+    # agent = SimpleCarAgent.from_file('filename.txt')
+    # # создаёт мир
+    # w = SimpleCarWorld(1, m, SimplePhysics, SimpleCarAgent, timedelta=0.2)
+    # # подключает к нему агента
+    # w.set_agents([agent])
+    # # и запускается
+    # w.run()
+    # # или оценивает агента в этом мире
+    # print(w.evaluate_agent(agent, 500))
