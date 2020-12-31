10388bb65cea42f140431ce69fa1757188c15378
Boris Gribovskiy <xinu@yandex.ru>
2020-12-31 16:49:24 +0300

    v0.1.1 published to PYPI

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup
+import setuptools
 
-setup(name='gym_grand_prix',
-      version='0.1.0',
+setuptools.setup(name='gym-grand-prix',
+      version='0.1.1',
       description='Gym GrandPrix Env',
       url='https://github.com/boangri/gym-grand-prix',
       author='Boris Gribovskiy',
-      packages=['gym_grand_prix'],
+      packages=setuptools.find_packages(),
       author_email='xinu@yandex.ru',
       license='MIT License',
-      install_requires=['gym', 'numpy', 'pygame']
-      )
+      install_requires=['gym', 'numpy', 'pygame'],
+      python_requires='>=3.6'
+)
